18275f3fd239b57e669ad8a8c159acecb67d37b8
Piest170 <PU170@hotmail.com>
2023-08-08 14:08:22 +0700

    Set Up Config to Connect SQL Server & Create Database.

--- eTicket/Program.cs
+++ eTicket/Program.cs
@@ -1,10 +1,12 @@
 using eTicket.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>();
+// Database Configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
