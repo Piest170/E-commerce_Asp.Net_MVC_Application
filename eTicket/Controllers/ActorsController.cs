ad5c29925db1e5b084678052fdbdfb40a2f5bdee
Piest170 <PU170@hotmail.com>
2023-08-15 08:22:15 +0700

    Add ActorsService

--- eTicket/Controllers/ActorsController.cs
+++ eTicket/Controllers/ActorsController.cs
@@ -1,19 +1,20 @@
 ﻿using eTicket.Data;
+using eTicket.Data.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eTicket.Controllers
 {
     public class ActorsController : Controller
     {
-        private readonly AppDbContext _context;
+        private readonly IActorsService _service;
 
-        public ActorsController(AppDbContext context)
+        public ActorsController(IActorsService service)
         {
-            _context = context;
+            _service = service;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var allActors = _context.Actors.ToList();
+            var allActors = await _service.GetAll();
             return View(allActors);
         }
     }
